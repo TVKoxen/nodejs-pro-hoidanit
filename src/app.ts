c9a1e77efa4ae075e7384cac87474d2f8a1ad835
nguyentt <nguyenthanhtran01@gmail.com>
2025-06-14 15:38:44 +0700

    video 30

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 //const express = require("express");
 import express from "express";
+import 'dotenv/config'
+//require('dotenv').config(); dùng nào cũng được, import thì chỉ typesript được
 const PORT = 8080;
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Thanh Nguyen Tran");
+    res.send("Hello World nodemon");
 })
 
 app.get("/hoidanit", (req, res) => {
@@ -14,4 +16,5 @@ app.get("/hoidanit", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`My app is running on port: ${PORT}`);
+    console.log("env port: ", process.env.PORT);
 })
